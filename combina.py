d7a0f219f1504b5deb07d27f933f677896cca1df
Jacques Pienaar <jacques.pienaar@zoho.com>
2020-11-21 18:50:46 -0500

    updated stuff

--- combina.py
+++ combina.py
@@ -32,16 +32,30 @@ def similar_names(a, b, threshold = 2):
     return names1, names2, multimatch1, multimatch2
 
 def main():
+    print('Olá! Você está usando combina.py, '
+          'desenvolvido por Jacques Pienaar, \n'
+          'uma programa simples para fundir '
+          'tabelas de notas e provas, \nusando '
+          'os nomes dos estudantes. \n'
+          'A programa está começando...')
     # Get data from files:
-    
-    filepath = os.getcwd()
-
-    notas = pd.read_csv(filepath + '\\notas.csv', encoding='latin-1')
-
-    provas = pd.read_csv(filepath + '\\provas.csv', encoding='latin-1')
-
+    print('Procurando arquivos notas.csv e provas.csv ...', end='')
+    try:
+        filepath = os.getcwd()
+        notas = pd.read_csv(filepath + '\\notas.csv', 
+                            encoding='latin-1')
+        provas = pd.read_csv(filepath + '\\provas.csv', 
+                             encoding='latin-1')
+    except FileNotFoundError:
+        print('Erro: Não achei os arquivos notas.csv e provas.csv .'
+              '\nPor favor verifique que os arquivos estão nomeado '
+              'corretamente, e que eles ficam '
+              'na pasta {}'.format(filepath))
+        ending = input('(Digite qualquer tecla para sair.) ')
+        sys.exit()
+    print(' successo.')
     # Tidy up the data:
-
+    print('Organizando os dados...', end='')
     provas = provas.dropna(axis=0,how='all')
     provas = provas.dropna(axis=1,how='all')
     provas.rename(columns={provas.columns[0]:'Nome', 
@@ -70,56 +84,66 @@ def main():
     # Check that no IDs are duplicated:
     notas_IDs = notas.dropna(subset=['ID'])
     provas_IDs = provas.dropna(subset=['ID'])
-    assert len(notas_IDs['ID'].unique()) == len(notas_IDs)
-    assert len(provas_IDs['ID'].unique()) == len(provas_IDs)
-
+    try:
+        assert len(notas_IDs['ID'].unique()) == len(notas_IDs)
+        assert len(provas_IDs['ID'].unique()) == len(provas_IDs)
+    except AssertionError:
+        print('Erro: Algumas números de identificação estão '
+              'repetidos. \nVerifique que cada número parece '
+              'somente um vez em cada tabela, e tente de novo.')
+        ending = input('(Digite qualquer tecla para sair.) ')
+        sys.exit()
 
     # If a name has duplicates in one of the lists, ensure
     # all instances of it in that list are accompanied by IDs:
 
     n_count = notas.Nome.value_counts()
     n_multi = n_count[n_count >1]
+    # TODO: rewrite using try/except
     for n in n_multi:
         problem = len(notas[(notas.Nome == n)&(notas['ID'].isna())])
         if problem:
-            sys.exit('Erro: o nome {} parece multiplus vezes no arquivo "notas". '
-                     'Por favor '
+            print('Erro: o nome {} parece multiplus vezes no arquivo "notas". '
+                     '\nPor favor '
                      'coloque números de identificação distintos para eles '
                      'e tente de novo.'.format(n))
+            ending = input('(Digite qualquer tecla para sair.) ')
+            sys.exit()
         
     p_count = provas.Nome.value_counts()
     p_multi = p_count[p_count >1]        
     for p in p_multi:
-        problem = len(provas[(provas.Nome == n)&(provas['ID'].isna())])
+        problem = len(provas[(provas.Nome == p)&(provas['ID'].isna())])
         if problem:
-            sys.exit('Erro: o nome {} parece multiplus vezes no arquivo "provas". '
-                     'Por favor '
+            print('Erro: o nome {} parece multiplus vezes no arquivo "provas". '
+                     '\nPor favor '
                      'coloque números de identificação distintos para eles '
                      'e tente de novo.'.format(n))
-
+            ending = input('(Digite qualquer tecla para sair.) ')
+            sys.exit()
+    print(' successo.')
     # Assign unique IDs (UIDs) based on common IDs:
-
+    print('Comparando os nomes dos estudantes ...')
     common_ID = pd.merge(notas_IDs, provas_IDs, how='inner', on='ID',
                          suffixes=('_n','_p')) 
-
     UID = 0
     notas = notas.assign(UID = np.nan)
     provas = provas.assign(UID = np.nan)
     for i in common_ID.ID:
-        notas.UID[notas.ID == i] = UID
-        provas.UID[provas.ID == i] = UID
+        notas.loc[notas.ID == i,'UID'] = UID
+        provas.loc[provas.ID == i, 'UID'] = UID
         UID += 1
         
     # Assign UIDs based on similar names:
     notas = notas.assign(nameid = notas.Nome)
     notas.ID = notas.ID.apply(str)
-    notas.ID[notas.ID == 'nan'] = 'desconhecido'
-    notas.nameid += ' (ID: ' + notas.ID + ')'
+    notas.loc[notas.ID == 'nan', 'ID'] = 'desconhecido'
+    notas.nameid += ' (ID:--' + notas.ID + ')'
 
     provas = provas.assign(nameid = provas.Nome)
     provas.ID = provas.ID.apply(str)
-    provas.ID[provas.ID == 'nan'] = 'desconhecido'
-    provas.nameid += ' (ID: ' + provas.ID + ')'
+    provas.loc[provas.ID == 'nan', 'ID'] = 'desconhecido'
+    provas.nameid += ' (ID:--' + provas.ID + ')'
 
     # Get name lists excluding those which already have a UID
     n_names = notas.nameid[notas.UID.isna()].to_list()
@@ -136,12 +160,15 @@ def main():
     # p column is the correct match.
     # Remove the false matches. Then repeat the process
     # for the p column.
+    # TODO: Exclude matches with incompatible IDs
+    # TODO: Give an option to say "Não sei".
 
     for n in n_multi:
         matches = pairs.p[pairs.n == n].to_list()
         num_options = len(matches)+1
-        print('O nome {} corresponde com qual das'
-              ' seguintes pessoas?'.format(n))
+        print('Me ajuda, por favor. O nome {} no arquivo notas.csv \n'
+              'corresponde com qual das seguintes pessoas'
+              'no arquivo provas.csv? \nEscolhe um:'.format(n))
         for i in range(len(matches)):
             print('({}) {} '.format(i+1,matches[i]) )
         print('({}) Nenhuma deles acima.'.format(num_options))
@@ -153,11 +180,9 @@ def main():
                 print('Por favor, insira um númer entre '
                       '1 e {}.'.format(len(matches)+1))
                 ans = False
-        print('ans = {}, num_opts = {}'.format(ans,num_options))
         if ans == num_options:
             pairs = pairs[pairs.n != n]  
         else:
-            print('else statement activated')
             p = matches[ans-1]
             pairs = pairs[((pairs.n != n) & (pairs.p != p))     
                           | ((pairs.n == n) & (pairs.p == p))]  
@@ -167,8 +192,9 @@ def main():
     for p in p_multi:
         matches = pairs.n[pairs.p == p].to_list()
         num_options = len(matches)+1
-        print('O nome {} corresponde com qual das'
-              ' seguintes pessoas?'.format(p))
+        print('Me ajuda, por favor. O nome {} no arquivo notas.csv \n'
+              'corresponde com qual das seguintes pessoas'
+              'no arquivo provas.csv? \nEscolhe um:'.format(p))
         for i in range(len(matches)):
             print('({}) {} '.format(i+1,matches[i]) )
         print('({}) Nenhuma deles acima.'.format(num_options))
@@ -183,11 +209,9 @@ def main():
                 print('Por favor, insira um númer entre '
                       '1 e {}.'.format(len(matches)+1))
                 ans = False
-        print('ans = {}, num_opts = {}'.format(ans,num_options))
         if ans == num_options:
             pairs = pairs[pairs.p != p] 
         else:
-            print('else statement activated')
             n = matches[ans-1]
             pairs = pairs[((pairs.n != n) & (pairs.p != p))     
                           | ((pairs.n == n) & (pairs.p == p))]  
@@ -196,26 +220,27 @@ def main():
         
     for n in pairs.n:
         for p in pairs.p[pairs.n == n]:
-            notas.UID[notas.nameid == n] = UID
-            provas.UID[provas.nameid == p] = UID
+            notas.loc[notas.nameid == n, 'UID'] = UID
+            provas.loc[provas.nameid == p, 'UID'] = UID
             UID += 1
             
     # ...and give unique UIDs to the remaining students:
         
     x = notas.UID[notas.UID.isna()]
     UIDs = [UID+i for i in range(len(x))]
-    notas.UID[notas.UID.isna()] = UIDs
+    notas.loc[notas.UID.isna(), 'UID'] = UIDs
     UID += len(x)
     
     y = provas.UID[provas.UID.isna()]
     UIDs = [UID+i for i in range(len(y))]
-    provas.UID[provas.UID.isna()] = UIDs
+    provas.loc[provas.UID.isna(), 'UID'] = UIDs
     UID += len(y) 
 
     # Combine the data:
+    print('Obrigado! Estou fundindo as tabelas...', end='')
     # Restore the IDs
-    notas.ID[notas.ID == 'desconhecido'] = np.nan
-    provas.ID[provas.ID == 'desconhecido'] = np.nan
+    notas.loc[notas.ID == 'desconhecido', 'ID'] = np.nan
+    provas.loc[provas.ID == 'desconhecido', 'ID'] = np.nan
 
     # Fill in any missing ID numbers:
     
@@ -229,8 +254,8 @@ def main():
             u_ids[u] = np.nan
     
     for u in range(UID):
-        notas.ID[notas.UID == u] = u_ids[u]
-        provas.ID[provas.UID == u] = u_ids[u]
+        notas.loc[notas.UID == u, 'ID'] = u_ids[u]
+        provas.loc[provas.UID == u, 'ID'] = u_ids[u]
 
     # Make final merged table
         
@@ -246,9 +271,16 @@ def main():
     upfront_cols = ['Nome_1','Nome_2','ID']
     new_columns = upfront_cols + final.columns.drop(upfront_cols+['UID']).tolist()
     final = final[new_columns]
-
+    
     # save to output file
-    final.to_csv(filepath + '\\combined.csv', na_rep = '#N/A')
+    print(' successo. \nSalvando o arquivo...', end='')
+    final.to_csv(filepath + '\\combined.csv', na_rep = '#N/A', encoding='latin-1')
+    print(' successo.')
+    print('Pode achar o novo tabela no '
+          'arquivo {}\\combined.csv . \n'
+          'A programa está terminado. Obrigado.'.format(filepath))
+    ending = input('(Digite qualquer tecla para sair.) ')
+    sys.exit()
 
 if __name__ == '__main__':
   main()
